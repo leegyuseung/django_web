35b1907e14a917f458aec0da6058327f98596458
gyuseung lee <lgs4002@naver.com>
2023-01-16 21:00:54 +0900

    장고 admin을 통한 데이터 관리

--- instagram/models.py
+++ instagram/models.py
@@ -3,5 +3,17 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # Java의 toString
+    def __str__(self):
+        # return "Custom Post Object({})".format(self.id)
+        # return f"Custom Post object ({self.id})"
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+    #
+    # message_length.short_description = "메세지 글자수"

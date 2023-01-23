54252448dabf6a32941dbab30beb4bc327d70081
gyuseung lee <lgs4002@naver.com>
2023-01-23 22:13:54 +0900

    Queryset의 정렬 및 범위 조건

--- instagram/models.py
+++ instagram/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
         # return f"Custom Post object ({self.id})"
         return self.message
 
+    class Meta:
+        ordering = ["-id"]
+
     # def message_length(self):
     #     return len(self.message)
     #
